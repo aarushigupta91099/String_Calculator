31c6ec8ba3e4abb3cc962b1f6a99eb637f9c9f19
aarushi91099 <aarushigupta777@gmail.com>
2025-09-26 14:29:50 +0530

    adding integration for 2 input

--- src/main/java/com/example/stringCalcuator/service/StringCalculatorService.java
+++ src/main/java/com/example/stringCalcuator/service/StringCalculatorService.java
@@ -2,10 +2,18 @@ package com.example.stringCalcuator.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
 public class StringCalculatorService {
 
     public int add(String numbers) {
-        return numbers.isEmpty() ? 0 : Integer.parseInt(numbers);
+        if(numbers.isEmpty()){
+            return 0;
+        }
+        String[] num = numbers.split(",");
+        int numSum = 0;
+        numSum = Arrays.stream(num).mapToInt(Integer::parseInt).sum();
+        return numSum;
     }
 }
