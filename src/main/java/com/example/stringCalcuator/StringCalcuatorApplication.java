21280089c24fde26ed13ac970c2dd9516ae246bb
aarushi91099 <aarushigupta777@gmail.com>
2025-09-26 13:38:59 +0530

    Initialisation

---
+++ src/main/java/com/example/stringCalcuator/StringCalcuatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.stringCalcuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringCalcuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringCalcuatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stringCalcuator/StringCalcuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stringCalcuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringCalcuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
